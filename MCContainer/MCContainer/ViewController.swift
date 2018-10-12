596fcf12f5dc7925bc5fe3eb9c46d82ea377ffa2
AlexKostiev <akostiev@griddynamics.com>
2018-10-12 17:25:05 +0300

    Updated gitignore to exclude Pods/ directory from the version control

--- MCContainer/MCContainer/ViewController.swift
+++ MCContainer/MCContainer/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import MCComponent
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        guard let reusableButtonView = ReusableButtonView.fromNib() else { return }
+        reusableButtonView.backgroundColor = .green
+        view.addSubview(reusableButtonView)
+        
+        view.centerXAnchor.constraint(equalTo: reusableButtonView.centerXAnchor).isActive = true
+        reusableButtonView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        view.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        view.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        reusableButtonView.buttonTapHandler = { reusableButtonView in
+            print("button was tapped!")
+        }
     }
 
     override func didReceiveMemoryWarning() {
--- Podfile
+++ Podfile
@@ -1,5 +1,7 @@
 platform :ios, '9.0'
 project './MCContainer/MCContainer.xcodeproj'
+use_frameworks!
+
 target 'MCContainer' do
 	pod 'MCComponent', :git => 'https://github.com/AlexKostiev/MCComponent.git', :branch => 'master'
 end
